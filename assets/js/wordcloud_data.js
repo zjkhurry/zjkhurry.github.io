bc9640faae91a77eeed2a1388929e152e1222dd3
zjkhurry <18690762+zjkhurry@users.noreply.github.com>
2026-04-28 02:40:27 +0000

    Updated Word Cloud Data

--- assets/js/wordcloud_data.js
+++ assets/js/wordcloud_data.js
@@ -11,7 +11,7 @@ function initWordCloud() {
     function resizeContainer() {
         const width = container.parentElement.offsetWidth;
         container.style.height = (width * 0.5) + 'px';
-        // container.style.minHeight = '300px';
+        container.style.minHeight = '300px';
     }
 
     // 初始化时设置尺寸
@@ -32,27 +32,11 @@ function initWordCloud() {
             }
         }, 200); // 防抖，避免频繁重绘
     });
+
+    // 定义词云配置
     var wordCloudConfig = {
         list: [
-            ['Wearable Electronics', 100],
-            ['Sensors', 84],
-            ['Flexibility', 80],
-            ['Health Monitoring', 52],
-            ['Energy Harvesting', 52],
-            ['Skin-Interface', 28],
-            ['Nanotechnology', 28],
-            ['Haptic Interfaces', 24],
-            ['Haptic Feedback', 24],
-            ['Machine Learning', 20],
-            ['Flexible Electronics', 20],
-            ['Thermal Regulation', 16],
-            ['Cell Biology', 16],
-            ['Bioelectronics', 12],
-            ['Soft Robotics', 12],
-            ['Neuromorphic', 10],
-            ['Microfluidics', 10],
-            ['AR/VR', 10],
-            ['Biomedical Engineering', 10]
+            {word_list_str}
         ],
         weightFactor: 10,
         effect: 'linerMap',
@@ -64,11 +48,11 @@ function initWordCloud() {
         },
         color: [
             '#00BFFF',
-    '#1E90FF',
-    ['#87CEFA', '#1E90FF'],  // 天蓝渐变
+            '#1E90FF',
+            ['#87CEFA', '#1E90FF'],  // 天蓝渐变
         ],
         autoFontSize: true,
-        cursorWhenHover: 'pointer'
+        cursorWhenHover: 'pointer',
     };
 
     // 初始化词云
